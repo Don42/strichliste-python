ae9b9aa9c1b97aa7bd36893e034dd791713af37d
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-01-16 03:05:17 +0100

    Add argument parsing/checking

--- strichliste.py
+++ strichliste.py
@@ -1,3 +1,4 @@
+import numbers
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
@@ -7,6 +8,9 @@ api = Api(app)
 user_parser = reqparse.RequestParser()
 user_parser.add_argument('name')
 user_parser.add_argument('mailAddress')
+
+transaction_parser = reqparse.RequestParser()
+user_parser.add_argument('value')
 
 
 class Setting(Resource):
@@ -22,23 +26,36 @@ class UserList(Resource):
 
     def post(self):
         args = user_parser.parse_args()
-        print(args)
+        if not {'mailAddress', 'name'}.issubset(args):
+            return create_error(400, "name missing")
         return {'id': 1, 'name': 'gert', 'balance': 0, 'lastTransaction': None}
 
 
 class User(Resource):
 
     def get(self, user_id):
-        return {'message': "user {} not found".format(user_id)}, 404
+        return create_error(404, "user {} not found".format(user_id))
 
 
 class UserTransaction(Resource):
 
     def get(self, user_id):
-        return {'message': "user {} not found".format(user_id)}, 404
+        return create_error(404, "user {} not found".format(user_id))
 
     def post(self, user_id):
-        return {'message': "user {} not found".format(user_id)}, 404
+        args = user_parser.parse_args()
+        if not {'value'}.issubset():
+            return create_error(400, "value missing")
+        value = args['value']
+        if not isinstance(value, numbers.Number):
+            return create_error(400, "not a number: {}".format(value))
+        if value == 0:
+            return create_error(400, "value must not be zero")
+        return create_error(404, "user {} not found".format(user_id))
+
+
+def create_error(code: int, msg: str):
+    return {'message': msg}, code
 
 
 class Transaction(Resource):
