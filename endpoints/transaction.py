9240fae60a40fad529f0a0554c3341e3867ce9b5
Marco 'don' Kaulea <DonMarco42@gmail.com>
2016-01-31 16:52:47 +0100

    Add logging to UserTransaction

--- endpoints/transaction.py
+++ endpoints/transaction.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from flask_restful import Resource
 
 from endpoints import make_error_response, make_response, list_parser
@@ -9,9 +10,12 @@ class UserTransaction(Resource):
     def get(self, user_id, transaction_id):
         user = models.User.query.get(user_id)
         if user is None:
+            current_app.logger.warning("Could not find transaction: User ID not found - user_id='{}'".format(user_id))
             return make_error_response("user {} not found".format(user_id), 404)
         transaction = models.Transaction.query.get(transaction_id)
         if transaction is None or transaction.userId != user.id:
+            current_app.logger.warning(("Could not find transaction: User ID does not match - "
+                                        "user_id='{}', transaction_id='{}'").format(user_id, transaction_id))
             return make_error_response("transaction {} not found".format(transaction_id), 404)
         return make_response(transaction.dict(), 200)
 
